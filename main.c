c44cd2f0711d2948bf5b26cfcb3993d1b9c2ff76
Guillermo Gutiérrez <xiterrex@gmail.com>
2016-04-19 18:25:03 +0200

    corrijo getline (gnu) -> fgets (estandar)

--- main.c
+++ main.c
@@ -15,38 +15,28 @@ int main(void)
 	/* ===== Ask for world size ===== */
 
 	int rows = 0, cols = 0, density = 0;
-	/*
-	 * I use geline() instead of scanf() to accept default values using
-	 * the Enter key. Because of that, I must use dynamically allocated
-	 * buffer with the possibility to grow (according to getline spec).
-	 */
-	size_t buf_sz = 50;
-	char *buf = calloc(buf_sz, sizeof(char));
+	char buf[50];
 
 	printf("Numero de filas? [%d]: ", DEFAULT_ROWS);
 	fflush(stdout);
-	getline(&buf, &buf_sz, stdin);
+	fgets(buf, 50, stdin);
 	rows = atoi(buf);
 	if (rows <= 0)
 		rows = DEFAULT_ROWS;
 
 	printf("Numero de columnas? [%d]: ", DEFAULT_COLS);
 	fflush(stdout);
-	getline(&buf, &buf_sz, stdin);
+	fgets(buf, 50, stdin);
 	cols = atoi(buf);
 	if (cols <= 0)
 		cols = DEFAULT_COLS;
 
 	printf("%% de densidad de poblacion viva inicial? [%d]: ", DEFAULT_DENSITY);
 	fflush(stdout);
-	getline(&buf, &buf_sz, stdin);
+	fgets(buf, 50, stdin);
 	density = atoi(buf);
 	if (density <= 0)
 		density = DEFAULT_DENSITY;
-
-	free(buf);
-	buf_sz = 0;
-	buf = NULL;
 
 	/* ===== Run program ===== */
 
